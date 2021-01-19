c30373240db8bb4cbf61d9aa5e4b9290f12e89ac
Andrew Monshizadeh <amonshiz@gmail.com>
2021-01-18 23:20:50 -0500

    Add logging for pr, token, and depends on

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const wait = require('./wait');
 
 
 // most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+const run = async () => {
+  const pullRequest = github.context.payload.pull_request
+  if (!pullRequest) {
+    core.setFailed("Not a pull request");
+    return;
   }
-}
+
+  const token = core.getInput('github-token', {required: true})
+  if (!token) {
+    core.setFailed("Missing GITHUB_TOKEN");
+    return;
+  }
+
+  const dependsOnRegex = /Depends on: #?(?<parentpr>[0-9]+)/g;
+  const parentPrMatch = pullRequest.body.match(dependsOnRegex);
+  if (Object.keys(parentPrMatch.groups).length < 1) {
+    core.setFailed("No parent PR to traverse");
+    return;
+  }
+
+  console.log(`event payload: ${JSON.stringify(github.context.payload, undefined, 2)}`)
+  //
+  // try {
+  //   const ms = core.getInput('milliseconds');
+  //   core.info(`Waiting ${ms} milliseconds ...`);
+  //
+  //   core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+  //   await wait(parseInt(ms));
+  //   core.info((new Date()).toTimeString());
+  //
+  //   core.setOutput('time', new Date().toTimeString());
+  // } catch (error) {
+  //   core.setFailed(error.message);
+  // }
+};
 
 run();
